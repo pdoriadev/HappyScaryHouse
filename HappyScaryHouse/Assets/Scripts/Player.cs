dfe103591a5fba0523c8bc53bd00578ad604b36d
Pita Bread <pdoriadev@gmail.com>
2020-09-19 01:52:58 -0700

    Added simple camera follow

--- HappyScaryHouse/Assets/Scripts/Player.cs
+++ HappyScaryHouse/Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
     private Rigidbody2D RB2D;
---
+++ HappyScaryHouse/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class PlayerCamera : MonoBehaviour
+{
+    private Camera Cam;
+    private Vector3 velocity = Vector3.zero;
+
+    [SerializeField]
+    private Transform Target = default;
+    [SerializeField]
+    private Vector3 Offset = default;
+    [SerializeField]
+    private float SmoothDampTime = 0.3f;
+
+    void Awake()
+    {
+        Cam = GetComponent<Camera>();
+        if (Target == null)
+            Debug.LogError("Target is null");
+        Offset = Cam.transform.position - Target.position;
+    }
+
+
+    void LateUpdate()
+    {
+        Vector3 nextPos = Target.position + Offset;
+        Cam.transform.position = Vector3.SmoothDamp(transform.position, nextPos, ref velocity, SmoothDampTime);
+    }
+
+
+}
