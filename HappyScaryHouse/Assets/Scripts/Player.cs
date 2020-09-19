78bb5f891a9bcc2545182c6140bc6718829135f3
Pita Bread <pdoriadev@gmail.com>
2020-09-19 01:31:05 -0700

    Implemented player movement. Added PrototypeScene

---
+++ HappyScaryHouse/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D RB2D;
+    private Vector2 MoveDir;
+
+    public float moveSpeed;
+
+    void Awake()
+    {
+        RB2D = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        MoveDir.x = Input.GetAxisRaw("Horizontal");
+        MoveDir.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        RB2D.velocity = MoveDir * moveSpeed;
+    }
+}
