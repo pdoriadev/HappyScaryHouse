2a67fffd5a2f85b4f1787b0eb82475df9101914a
Pita Bread <pdoriadev@gmail.com>
2020-09-25 03:57:10 -0700

    Added GrannyAnimation. Installed 2D Tilemap

---
+++ HappyScaryHouse/Assets/Scripts/Granny.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Granny : MonoBehaviour, IInteractable
+{
+    [SerializeField]
+    private Animator Animator;
+
+    public void Interact()
+    {
+
+    }
+    public void CancelInteract()
+    {
+
+    }
+    public MonoBehaviour GetMonoBehaviour()
+    {
+        return this;
+    }
+
+    private void Awake()
+    {
+        Animator = GetComponentInChildren<Animator>();
+    }
+}
