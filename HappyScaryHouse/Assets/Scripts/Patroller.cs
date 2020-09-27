bb112c9b62f585d98b2a7121ba008e69b45952e7
Pita Bread <pdoriadev@gmail.com>
2020-09-27 08:18:22 -0700

    Patroller.cs - Sets transform position to patrol to predetermined array of points in   scene. Used for power-up pick-up

---
+++ HappyScaryHouse/Assets/Scripts/Patroller.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patroller : MonoBehaviour
+{
+    public float curveStrength = 10;
+    public float minDist = 2;
+    public float smoothTime = 1;
+    [SerializeField]
+    private Transform[] PatrolPoints = default;
+    private Transform TargetPoint;
+    private int NextPointIndex;
+
+
+    void Awake()
+    {
+        if (PatrolPoints.Length == 0)
+        {
+            Debug.LogError("No patrol points to patrol to for " + gameObject.name);
+        }
+    }
+    void Start()
+    {
+        float closestDist = float.MaxValue;
+        for (int i = 0; i < PatrolPoints.Length; i++)
+        {
+            float dist = (transform.position - PatrolPoints[i].position).sqrMagnitude;
+            if (dist < closestDist)
+            {
+                closestDist = dist;
+                TargetPoint = PatrolPoints[i];
+                NextPointIndex = i + 1 < PatrolPoints.Length ? ++i  : 0;
+            }
+        }
+
+    }
+
+    Vector3 Velocity = Vector3.zero;
+    Vector3 CurvePos = Vector3.forward;
+    void FixedUpdate()
+    {
+  
+        float distToTargetPoint = (transform.position - TargetPoint.position).sqrMagnitude;
+        Vector3 newPos = Vector3.zero;
+        
+        if (distToTargetPoint < minDist * minDist)
+        {
+            // #TODO  - add curve functionality
+            TargetPoint = PatrolPoints[NextPointIndex];
+            NextPointIndex = NextPointIndex + 1 < PatrolPoints.Length ? ++NextPointIndex : 0;
+        }
+        newPos = Vector3.SmoothDamp(transform.position, TargetPoint.position, ref Velocity, smoothTime) ;
+
+        Debug.Log(newPos);
+        transform.position = newPos;
+
+ 
+    }   
+}
